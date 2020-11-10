fc45fff63239c436bafec8c2dc4534a7c808f0d9
DarioBonini <66188969+DarioBonini@users.noreply.github.com>
2020-11-10 13:40:48 +0100

    BUGFIX

--- ServerUtil/099-ReindexDB-ReadMe.txt
+++ ServerUtil/099-ReindexDB-ReadMe.txt
@@ -6,5 +6,17 @@ https://go.microsoft.com/fwlink/?linkid=2142258
 
 2)
 verificare conf script .bat (percorso)
+
+3) - OPZIONALE
+scaricare ed installare Scaricare SQL Server Management Studio (SSMS)
+https://docs.microsoft.com/it-it/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15
+
+4) - OPZIONALE
+lanciare menagemnt studio come admin
+collegarsi al DB usando questa stringa
+\\.\pipe\MICROSOFT##WID\tsql\query
+
+
+99)
 lanciare script .bat
 
--- ServerUtil/100-ReindexDB.bat
+++ ServerUtil/100-ReindexDB.bat
@@ -1,3 +1,7 @@
+echo originale da sito MS
 echo sqlcmd -S np:\\.\pipe\MSSQL$MICROSOFT##SSEE\sql\query –i <scriptLocation>\WsusDBMaintenance.sql
+echo modifca trovata in rete
+echo "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\sqlcmd" -I -S \.\pipe\MICROSOFT##WID\tsql\query -i C:\Scripts\WSUSDBMaintenance.sql
 
-sqlcmd -S np:\\.\pipe\MSSQL$MICROSOFT##SSEE\sql\query –i "C:\_\ddgsms4update\ServerUtil\101-ReindexDB.sql"
+echo non funziona - sqlcmd -I -S \.\pipe\MICROSOFT##WID\tsql\query -i "C:\_\ddgsms4update\ServerUtil\101-ReindexDB.sql"
+sqlcmd -I -S \\.\pipe\MICROSOFT##WID\tsql\query -i "C:\_\ddgsms4update\ServerUtil\101-ReindexDB.sql"
